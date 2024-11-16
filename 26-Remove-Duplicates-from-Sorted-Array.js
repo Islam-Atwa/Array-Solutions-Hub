cfb98fef6fd84fb2cc5c47a85519b2aa704907b6
Islam Atwa <160741057+Islam-Atwa@users.noreply.github.com>
2024-11-16 03:02:55 +0200

    [26] [Time Beats: 100.00%] [Memory Beats: 66.08%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.js
+++ 26-Remove-Duplicates-from-Sorted-Array.js
@@ -1,16 +1,20 @@
-/*
- *
+/**
  * @param {number[]} nums
  * @return {number}
-*/
+ */
 var removeDuplicates = function(nums) {
-    let i = 1;
-
-    for (let j =1; j < nums.length; j++) {
-        if (nums [j] !== nums[i -1]){
-            nums[i]= nums[j];
-            i++;
+    let index =1;
+    for(let j=1; j<nums.length; j++){
+        if (nums[j] !== nums[index - 1]){
+            nums[index] = nums[j];
+            index++;
         }
     }
-    return i;
+    return index;
 };
+
+// Solve the problem using 'set object'
+// var removeDuplicates = function(nums) {
+//     let uniqueArray = [...new Set(nums)];
+//     return uniqueArray;
+// };
