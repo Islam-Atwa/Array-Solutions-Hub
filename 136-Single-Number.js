ce112c82a96001ee0a77103c768b722b0257c7ce
Islam Atwa <160741057+Islam-Atwa@users.noreply.github.com>
2024-11-25 01:17:35 +0200

    [136] [Time Beats: 100.00%] [Memory Beats: 90.61%] - LeetPush

---
+++ 136-Single-Number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++){
+        result = result ^ nums[i];
+    }
+    return result;
+};
+
+// Explanation of the problem in Arabic:
+
+// https://www.youtube.com/watch?v=fAWcgIhxUM8
+
